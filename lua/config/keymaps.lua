1966dfc0cb5e00840583e193e18d239e90cad803
Pedro Piquero <4339072+gowikel@users.noreply.github.com>
2026-05-05 02:13:14 +0100

    Remove some clutter

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,6 +2,13 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+vim.keymap.del("n", "<leader>l") -- LazyVim
+vim.keymap.del("n", "<leader>L") -- LazyVim changelog
+vim.keymap.del("n", "<leader>K") -- Keywordprg
+vim.keymap.del("n", "<leader>S") -- Select scratch buffer
+vim.keymap.del("n", "<leader>?") -- Buffer keymaps (which-key)
+vim.keymap.del("n", "<leader>`") -- Switch to other buffer
+
 vim.keymap.set("t", "<D-\\><D-\\>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 vim.keymap.set("t", "<D-/><D-/>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
