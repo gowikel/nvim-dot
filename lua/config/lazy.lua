094cafd38b5b4c82c2437243540262ee7ab343ed
Pedro Piquero <4339072+gowikel@users.noreply.github.com>
2026-07-21 10:00:44 +0100

    master: Enable Java language support

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -24,6 +24,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.git" },
     { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.lang.java" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.lang.python" },
     -- { import = "lazyvim.plugins.extras.lang.ruby" },
