31609f495cdfa0975b8d75e9da9694d1cd990748
Pedro Piquero <4339072+gowikel@users.noreply.github.com>
2026-07-26 15:34:17 +0100

    master: Enable DAP core extra for nvim-metals
    nvim-metals calls setup_dap() unconditionally on attach, but the Scala
    extra declares nvim-dap optional, so startup warned until the DAP core
    extra was enabled.

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -18,6 +18,7 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.lang.ansible" },
     { import = "lazyvim.plugins.extras.lang.clojure" },
     { import = "lazyvim.plugins.extras.lang.docker" },
