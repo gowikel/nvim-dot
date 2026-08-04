54f73729c6f04b8ec22352e92f807c78586461be
Pedro Piquero <4339072+gowikel@users.noreply.github.com>
2026-08-04 14:58:10 +0100

    master: Use Option instead of Cmd for neocodeium keymaps
    Cmd (D-) bindings collided with terminal shortcuts.

--- lua/plugins/neocodeium.lua
+++ lua/plugins/neocodeium.lua
@@ -6,12 +6,12 @@ return {
       require("neocodeium").setup()
       local nc = require("neocodeium")
       local blink = require("blink.cmp")
-      vim.keymap.set("i", "<D-f>", function() blink.hide(); nc.accept() end)
-      vim.keymap.set("i", "<D-w>", function() blink.hide(); nc.accept_word() end)
-      vim.keymap.set("i", "<D-l>", function() blink.hide(); nc.accept_line() end)
-      vim.keymap.set("i", "<D-j>", function() blink.hide(); nc.cycle_or_complete() end)
-      vim.keymap.set("i", "<D-k>", function() blink.hide(); nc.cycle_or_complete(-1) end)
-      vim.keymap.set("i", "<D-c>", function() nc.clear() end)
+      vim.keymap.set("i", "<M-f>", function() blink.hide(); nc.accept() end)
+      vim.keymap.set("i", "<M-w>", function() blink.hide(); nc.accept_word() end)
+      vim.keymap.set("i", "<M-l>", function() blink.hide(); nc.accept_line() end)
+      vim.keymap.set("i", "<M-j>", function() blink.hide(); nc.cycle_or_complete() end)
+      vim.keymap.set("i", "<M-k>", function() blink.hide(); nc.cycle_or_complete(-1) end)
+      vim.keymap.set("i", "<M-c>", function() nc.clear() end)
     end,
   },
 }
